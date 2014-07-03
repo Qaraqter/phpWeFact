24f1ac2f1c56a48e9cb81d655dc6667f9ab71af7
Robin Rijkeboer <tunderofnl@gmail.com>
2014-07-03 16:15:00 +0200

    Added sendInvoiceReminderByEmail

--- lib/phpWeFact/WeFact.php
+++ lib/phpWeFact/WeFact.php
@@ -650,6 +650,24 @@ class API
         $parameters = get_object_vars($invoice);
         return $this->sendRequest('invoice', 'sendbyemail', $parameters);
     }
+
+    /**
+     * Send an invoice reminder by email
+     * @param  Invoice $invoice
+     * @return array
+     */
+    public function sendInvoiceReminderByEmail(Invoice $invoice)
+    {
+        if ($invoice->getInvoiceCode() == '') {
+            throw new \InvalidArgumentException(
+                sprintf('InvoiceCode must be defined!')
+            );
+        }
+
+        $parameters = get_object_vars($invoice);
+        return $this->sendRequest('invoice', 'sendreminderbyemail', $parameters);
+    }
+
      * sendRequest sends the request to the WeFact API
      * @param  string $controller
      * @param  string $action

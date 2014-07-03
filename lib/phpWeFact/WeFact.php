f56e094a6790abbe07808e40f78adcf96e170733
Robin Rijkeboer <tunderofnl@gmail.com>
2014-07-03 16:18:16 +0200

    Added showInvoice

--- lib/phpWeFact/WeFact.php
+++ lib/phpWeFact/WeFact.php
@@ -686,6 +686,23 @@ class API
     }
 
     /**
+     * Shows an invoice's details
+     * @param  Invoice $invoice
+     * @return array
+     */
+    public function showInvoice(Invoice $invoice)
+    {
+        if ($invoice->getInvoiceCode() == '') {
+            throw new \InvalidArgumentException(
+                sprintf('InvoiceCode must be defined!')
+            );
+        }
+
+        $parameters = get_object_vars($invoice);
+        return $this->sendRequest('invoice', 'sendsummationbyemail', $parameters);
+    }
+
+    /**
      * sendRequest sends the request to the WeFact API
      * @param  string $controller
      * @param  string $action
